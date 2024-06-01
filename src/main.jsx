b3fc1925ed6571231cd6204cad39163fb21bd765
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-01 09:20:05 +0600

    wrap router-provider with auth-provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render();
+import { AuthProvider } from "./providers/AuthProvider";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
