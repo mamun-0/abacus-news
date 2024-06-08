3c6482fcf8cbe26ecfb7f7cb069c1b2ec72bfd70
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-09 00:51:46 +0600

    ToastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import "./index.css";
 import { AuthProvider } from "./providers/AuthProvider";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <RouterProvider router={router} />
+    <ToastContainer autoClose={1000} />
   </AuthProvider>
 );
