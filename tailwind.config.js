77438d7796318d66c77225d739ee0febda27b281
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-16 00:33:32 +0600

    Add flowbit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/preline/dist/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin")],
+  plugins: [require("preline/plugin"), flowbite.plugin()],
 };
