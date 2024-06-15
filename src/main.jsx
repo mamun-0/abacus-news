6977ff95881949de90c60be2887d931217784165
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-15 21:35:52 +0600

    wrap with transtack query component

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,15 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
     <ToastContainer autoClose={1000} />
   </AuthProvider>
 );
