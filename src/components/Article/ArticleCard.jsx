7a53078b3120c95befee057451166d55a0ed4940
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-16 12:45:23 +0600

    Add premium badge

--- src/components/Article/ArticleCard.jsx
+++ src/components/Article/ArticleCard.jsx
@@ -1,15 +1,24 @@
+import { Badge } from "flowbite-react";
 import { Link } from "react-router-dom";
-
+import { HiCheck } from "react-icons/hi";
 export function ArticleCard({
   _id,
-  title = "",
-  image = "",
-  publisher = "",
-  description = "",
+  title,
+  image,
+  publisher,
+  description,
+  premium,
 }) {
   return (
-    <div className="h-96 w-80 border-2 p-1 flex flex-col justify-around">
-      <h2 className="text-xl text-center font-bold">{title}</h2>
+    <div
+      className={`${
+        premium ? "shadow-xl font-extralight" : ""
+      } h-96 w-80 border-2 rounded-md p-1 flex flex-col justify-around`}
+    >
+      <div className="flex justify-center">
+        <h2 className="text-xl text-center font-bold">{title}</h2>
+        {premium ? <PremiumBadge /> : ""}
+      </div>
       <img className="w-full h-3/5 object-cover" src={image} alt="" />
       <h2 className="text-lg">
         <span className="font-semibold">Publusher</span>: {publisher}
@@ -25,6 +34,14 @@ export function ArticleCard({
       >
         View Details
       </Link>
+    </div>
+  );
+}
+
+function PremiumBadge() {
+  return (
+    <div className="flex ml-2">
+      <Badge icon={HiCheck}>Premium</Badge>
     </div>
   );
 }
