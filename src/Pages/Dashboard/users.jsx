2665e005447818b49a1ea3682dc5c12cf687db29
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-20 20:51:51 +0600

    users page

---
+++ src/Pages/Dashboard/users.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAxiosSecure } from "../../hooks/useAxiosSecure";
+import { Heading } from "../../components/Heading/Heading";
+import { UsersTable } from "../../components/Dashboard/usersTable";
+
+export function Users() {
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: users = [],
+    error,
+    isPending,
+    refetch,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/user");
+      return data;
+    },
+  });
+  if (isPending) return "Loading";
+  if (error) return "Something went wrong";
+  async function handleAdmin(id) {
+    return axiosSecure.put(`/user/admin/${id}`);
+  }
+  return (
+    <div>
+      <Heading
+        title="All Users Dashboard"
+        subheading={`Total users : ${users.length}`}
+      />
+      <UsersTable users={users} handleAdmin={handleAdmin} refetch={refetch} />
+    </div>
+  );
+}
