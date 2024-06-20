6ffa1d39d180e19637f8b1af9b7d7de0da4f6bde
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-20 20:52:45 +0600

    allArticle Page

---
+++ src/Pages/Dashboard/allArticle.jsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import { ArticleTable } from "../../components/Dashboard/articleTable";
+import { Heading } from "../../components/Heading/Heading";
+import { useAxiosSecure } from "../../hooks/useAxiosSecure";
+
+export function AllArticle() {
+  const axiosSecure = useAxiosSecure();
+  async function handleUpdate(id, queryText, payload) {
+    const url = `/user/admin/permission/${id}?update=${queryText}`;
+    return axiosSecure.put(url, payload);
+  }
+  const {
+    data: allUsers,
+    error,
+    isPending,
+    refetch,
+  } = useQuery({
+    queryKey: ["AllArticle"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/user/admin/users");
+      return data;
+    },
+  });
+  if (isPending) return "Loading";
+  if (error) return "Something went wrong";
+  return (
+    <div className="space-y-2">
+      <Heading
+        title="All Article"
+        subheading={`Total Posts : ${allUsers.length}`}
+      />
+      {allUsers.map((user, idx) => {
+        return (
+          <ArticleTable
+            key={user._id}
+            idx={idx}
+            {...user}
+            handleUpdate={handleUpdate}
+            refetch={refetch}
+          />
+        );
+      })}
+    </div>
+  );
+}
