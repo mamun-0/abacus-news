1a1b69e917f4c5a20a6fba546190c80b28b2e5cd
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-21 17:56:48 +0600

    Modal after 10 seconds

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar/Navbar";
+import { ModalAfter10Sec } from "../components/ModalAfter10Sec";
 
 export function Main() {
   return (
     <div>
+      <ModalAfter10Sec />
       <Navbar />
       <div className="min-h-screen">
         <Outlet />
