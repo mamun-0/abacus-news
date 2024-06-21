b74ac538d5707e36e8bf11e41eba1e1485dfc1e2
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-21 12:04:45 +0600

    Publisher page

---
+++ src/Pages/Dashboard/publisher.jsx
@@ -0,0 +1,11 @@
+import { Publisher } from "../../components/Forms/Publisher";
+import { Heading } from "../../components/Heading/Heading";
+
+export function PublisherPage() {
+  return (
+    <div>
+      <Heading title="Add Publisher" />
+      <Publisher />
+    </div>
+  );
+}
