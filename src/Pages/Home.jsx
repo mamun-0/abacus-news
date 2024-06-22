f0ddeec0615ff5e178b0c01c8097ec2dd8d07cd8
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-22 13:31:14 +0600

    Publisher section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet";
 import { CarouselComponent } from "../components/Carousel";
+import { Heading } from "../components/Heading/Heading";
+import { PublisherSection } from "../components/PublisherSection";
 
 export function Home() {
   return (
@@ -8,6 +10,11 @@ export function Home() {
         <title>Home</title>
       </Helmet>
       <CarouselComponent />
+      <Heading
+        title="All The Publishers"
+        subheading="Vox: Insightful coverage of politics and culture. MSNBC: Progressive commentary on current affairs. Reuters: Impartial global news and finance. Al Jazeera: Comprehensive international coverage. BBC: Extensive global news and education."
+      />
+      <PublisherSection />
     </div>
   );
 }
