d834ddc3e88948a6c8e19776b38427490554ad71
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-06-22 14:49:16 +0600

    StatisticSection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet";
 import { CarouselComponent } from "../components/Carousel";
 import { Heading } from "../components/Heading/Heading";
 import { PublisherSection } from "../components/PublisherSection";
+import { StatisticSection } from "../components/StatisticSection";
 
 export function Home() {
   return (
@@ -15,6 +16,8 @@ export function Home() {
         subheading="Vox: Insightful coverage of politics and culture. MSNBC: Progressive commentary on current affairs. Reuters: Impartial global news and finance. Al Jazeera: Comprehensive international coverage. BBC: Extensive global news and education."
       />
       <PublisherSection />
+      <Heading title="Statistic"/>
+      <StatisticSection />
     </div>
   );
 }
---
+++ src/components/StatisticSection.jsx
@@ -0,0 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAxios } from "../hooks/useAxios";
+import CountUp from "react-countup";
+
+export function StatisticSection() {
+  const axiosCommon = useAxios();
+  const {
+    data: statistic,
+    error,
+    isPending,
+  } = useQuery({
+    queryKey: ["statistic"],
+    queryFn: async () => {
+      const { data } = await axiosCommon.get("/userCount");
+      return data;
+    },
+  });
+  if (isPending) return "Loading..";
+  const background_1 = {
+    background: `linear-gradient(45deg, #fad029 0%, #ff9a9e 100%)`,
+  };
+  const background_2 = {
+    background: `linear-gradient(90deg, #a1c4dd 0%, #C82249 100%)`,
+  };
+  const background_3 = {
+    background: `linear-gradient(135deg, #667eea 0%, #764ba2 100%)`,
+  };
+  console.log(statistic);
+  return (
+    <div className="mx-3 grid gap-4  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center">
+      <div
+        style={background_1}
+        className="h-36 bg-black w-full rounded-sm flex justify-center items-center"
+      >
+        <h2 className="text-2xl text-white font-bold">
+          Normal : <CountUp end={statistic?.normal || 0} duration={10} />
+        </h2>
+      </div>
+      <div
+        style={background_2}
+        className="h-36 bg-black w-full rounded-sm flex justify-center items-center"
+      >
+        <h2 className="text-2xl text-white font-bold">
+          {" "}
+          Premium : <CountUp end={statistic?.premium || 0} duration={10} />
+        </h2>
+      </div>
+      <div
+        style={background_3}
+        className="h-36 bg-black w-full rounded-sm flex justify-center items-center"
+      >
+        <h2 className="text-2xl text-white font-bold">
+          {" "}
+          Admin : <CountUp end={statistic?.admin || 0} duration={10} />
+        </h2>
+      </div>
+    </div>
+  );
+}
